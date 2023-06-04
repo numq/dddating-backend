d553ff0143defd8b174ad28778a62788ad44fffe
numq <numq.dev@gmail.com>
2023-06-04 16:40:19 +0300

    Provide status refactoring

--- library/status/src/lib.rs
+++ library/status/src/lib.rs
@@ -1,21 +1,20 @@
-use std::any::Any;
 use std::error::Error;
 
 use tonic::{Code, Response, Status as GrpcStatus};
 
-pub struct Status;
+pub struct Status<T>(T);
 
-impl Status {
-    pub fn unauthenticated(message: &str) -> Result<Response<dyn Any>, GrpcStatus> {
+impl<T> Status<T> {
+    pub fn unauthenticated(message: &str) -> Result<Response<T>, GrpcStatus> {
         Err(GrpcStatus::new(Code::Unauthenticated, message))
     }
 
-    pub fn invalid_arguments(args: Vec<&str>) -> Result<Response<dyn Any>, GrpcStatus> {
+    pub fn invalid_arguments(args: Vec<&str>) -> Result<Response<T>, GrpcStatus> {
         let message = format!("Invalid Arguments: {}", args.join(","));
         Err(GrpcStatus::new(Code::InvalidArgument, message))
     }
 
-    pub fn internal(error: Box<dyn Error>) -> Result<Response<dyn Any>, GrpcStatus> {
+    pub fn internal(error: Box<dyn Error>) -> Result<Response<T>, GrpcStatus> {
         Err(GrpcStatus::new(Code::Internal, error.to_string()))
     }
 }
@@ -26,20 +25,20 @@ mod tests {
 
     #[test]
     fn unauthenticated() {
-        let status = Status::unauthenticated("test");
+        let status = Status::<Response<()>>::unauthenticated("test");
         assert_eq!(status.unwrap_err().message(), "test");
     }
 
     #[test]
     fn invalid_argument() {
-        let status = Status::invalid_arguments(vec!["test"]);
+        let status = Status::<Response<()>>::invalid_arguments(vec!["test"]);
         assert_eq!(status.unwrap_err().message(), "Invalid Arguments: test");
     }
 
     #[test]
     fn internal() {
         let error = std::fmt::Error;
-        let status = Status::internal(Box::new(error));
+        let status = Status::<Response<()>>::internal(Box::new(error));
         assert_eq!(status.unwrap_err().message(), error.to_string());
     }
 }
