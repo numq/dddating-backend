6489206501dacb0113bcc14887dc81557d568162
numq <numq.dev@gmail.com>
2023-06-15 02:18:08 +0300

    Provide refactoring

--- library/amqp/src/lib.rs
+++ library/amqp/src/lib.rs
@@ -4,16 +4,17 @@ use amiquip::{Channel, Connection, ConsumerMessage, ExchangeDeclareOptions, Exch
 use serde::{de::DeserializeOwned, Serialize};
 
 pub struct MessageQueue {
-    url: String,
+    hostname: String,
+    port: String,
 }
 
 impl MessageQueue {
     fn get_connection(&self) -> Result<Connection, Box<dyn Error>> {
-        Connection::insecure_open(&format!("amqp://{}", self.url)).map_err(|e| Box::new(e) as Box<dyn Error>)
+        Connection::insecure_open(&format!("amqp://{}:{}", self.hostname, self.port)).map_err(|e| Box::new(e) as Box<dyn Error>)
     }
 
-    pub fn new(url: &str) -> Self {
-        Self { url: String::from(url) }
+    pub fn new(hostname: &str, port: &str) -> Self {
+        Self { hostname: String::from(hostname), port: String::from(port) }
     }
 
     pub fn bind_queue(
@@ -76,23 +77,24 @@ impl MessageQueue {
 mod tests {
     use super::*;
 
-    const URL: &str = "127.0.0.1:5672";
+    const HOSTNAME: &str = "127.0.0.1";
+    const PORT: &str = "5672";
 
     #[test]
     fn test_bind_publish_consume() {
         let (exchange_name, queue_name, routing_key, message) = ("exchange", "queue", "key", "message");
 
-        let queue = MessageQueue::new(URL);
+        let queue = MessageQueue::new(HOSTNAME, PORT);
         assert_eq!(queue.bind_queue(exchange_name, queue_name, routing_key).unwrap(), ());
 
         assert_eq!(queue.publish(exchange_name, routing_key, message).unwrap(), ());
 
-        fn handler(msg: String) -> bool {
+        fn handle(msg: String) -> bool {
             println!("{}", msg);
             assert_eq!(&msg, "message");
             false
         }
 
-        assert_eq!(queue.consume(queue_name, handler).unwrap(), ());
+        assert_eq!(queue.consume(queue_name, handle).unwrap(), ());
     }
 }
