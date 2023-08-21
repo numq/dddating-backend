7758755dc15d4b59bd7647e47174d3925e3b0bbc
numq <numq.dev@gmail.com>
2023-08-21 04:08:00 +0300

    Provide client changes

--- service/authentication/src/main.rs
+++ service/authentication/src/main.rs
@@ -1,7 +1,8 @@
 use std::error::Error;
 use std::net::SocketAddr;
+use std::time::Duration;
 
-use tonic::transport::{Channel, Server};
+use tonic::transport::{Channel, Server, Uri};
 
 use crate::account::pb::account_service_client::AccountServiceClient;
 use crate::token::pb::token_service_client::TokenServiceClient;
@@ -21,13 +22,13 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let create_channel_url: fn(&str, &str) -> &'static str = |hostname, port| Box::leak(format!("https://{}:{}", hostname, port).into_boxed_str());
 
     let account_channel_url = create_channel_url(&cfg.default_hostname.clone().unwrap(), &cfg.find_port(ACCOUNT_SERVICE_NAME).unwrap());
-    let account_channel = Channel::from_static(account_channel_url).connect().await?;
+    let account_channel = Channel::from_static(account_channel_url).connect_lazy();
     let account_client = AccountServiceClient::new(account_channel);
     let account_api = account::api::AccountApiImpl::new(account_client);
     let account_repository = account::repository::AccountRepositoryImpl::new(account_api);
 
     let token_channel_url = create_channel_url(&cfg.default_hostname.clone().unwrap(), &cfg.find_port(TOKEN_SERVICE_NAME).unwrap());
-    let token_channel = Channel::from_static(&token_channel_url).connect().await?;
+    let token_channel = Channel::from_static(token_channel_url).connect_lazy();
     let token_client = TokenServiceClient::new(token_channel);
     let token_api = token::api::TokenApiImpl::new(token_client);
     let token_repository = token::repository::TokenRepositoryImpl::new(token_api);
